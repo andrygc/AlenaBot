97c385837a09a3d14f1687ff4642b5126cd43eb7
Andry Noilien Guzmán Cardoza <65035621+andrygc@users.noreply.github.com>
2022-04-19 15:32:48 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- 
+          ___   _       _____   __   _       ___   _____   _____   _____  
+         /   | | |     | ____| |  \ | |     /   | |  _  \ /  _  \ |_   _| 
+        / /| | | |     | |__   |   \| |    / /| | | |_| | | | | |   | |   
+       / / | | | |     |  __|  | |\   |   / / | | |  _  { | | | |   | |   
+      / /  | | | |___  | |___  | | \  |  / /  | | | |_| | | |_| |   | |   
+     /_/   |_| |_____| |_____| |_|  \_| /_/   |_| |_____/ \_____/   |_|   
+
+                    ..::AlenaBot::..  INFORMATION PANEL
+.---------------------------------------------------------------------------.
+|       Developer: Andry Noilien Guzmán Cardoza                             |
+|       Phone: +5358511612                                                  |
+|       Email: andrynoilien123@nauta.cu | andrynoilien@gmail.com            |
+|       WhatsApp: +5358511612                                               |
+|       Telegram: +5358511612                                               |
+|       Info: http://www.facebook.com/Andry Noilien Guzmán Cardoza          | 
+|             http://www.twitter.com/@AndryCardoza                          |
+|       File: index.php                                                     |
+| ------------------------------------------------------------------------- |
+|       Company: CommunityWeb Inc.                                          |
+|       CommunityWeb (c) 2015-2016, Bad Boy. All Rights Reserved.           |
+|       CommunityWeb (c) 2016-2019, Bad Boy. All Rights Reserved.           |
+|       CommunityWeb (c) 2019-2021, Bad Boy. All Rights Reserved.           |
+'---------------------------------------------------------------------------'
+
+-->
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlenaBot</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="../static/img/favicon.png" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="chatbox">
+            <div class="header">
+                <center><h4><img src='img/AlenaBot.jpg' class='imgRedonda'/> AlenaBot <font size="1">v1.4</font></h4></center>
+            </div>
+            <div class="body" id="chatbody">
+                <p class="alena">Holaaa!!, soy Alena, asistente virtual de αℓα¢нαт, y estoy aqu&iacute; para ayudarte con cualquier duda que tengas acerca del sitio.</p>
+                <p class="alena">Ejecuta <i>/novedades</i> para ver nuevas funciones y <i>/help</i> para ver la ayuda.</p>
+                <div class="scroller"></div>
+            </div>
+            <div>
+                <input type="text" name="chat" id="chat" placeholder="Escribe un comando aqu&iacute;" style="font-size: 15px;">
+            </div>
+            <div>
+                <input type="submit" value="Enviar" id="btn">
+            </div>
+        </div>
+    </div>
+    
+<script src="js/app.js"></script>
+    
+</body>
+
+</html>
